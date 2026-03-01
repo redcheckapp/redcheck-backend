22d08ac0323ca775adf55e7708a2190c6a03a89f
Pacolias <pacomolinac2003@gmail.com>
2026-03-02 00:01:03 +0100

    feat: init Spring Boot project with MySQL connection

---
+++ src/main/java/com/redcheck/backend/RedCheckApiApplication.java
@@ -0,0 +1,13 @@
+package com.redcheck.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedCheckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedCheckApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redcheck/backend/RedCheckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redcheck.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedCheckApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
