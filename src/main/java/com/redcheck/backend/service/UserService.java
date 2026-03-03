179500b53e83d9ad253cc35273b0d94b1f6a9345
Pacolias <pacomolinac2003@gmail.com>
2026-03-03 19:49:27 +0100

    feat: UserService

--- src/main/java/com/redcheck/backend/service/UserService.java
+++ src/main/java/com/redcheck/backend/service/UserService.java
@@ -1,4 +1,24 @@
 package com.redcheck.backend.service;
 
+import com.redcheck.backend.entity.User;
+import com.redcheck.backend.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void deleteUser(String email){
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        userRepository.delete(user);
+    }
+
+
 }
