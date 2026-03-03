64a86ca09e1e592e523724750d17d3d4a97dd00a
Pacolias <pacomolinac2003@gmail.com>
2026-03-03 22:50:09 +0100

    feat: Subject

--- src/main/java/com/redcheck/backend/entity/Subject.java
+++ src/main/java/com/redcheck/backend/entity/Subject.java
@@ -1,4 +1,30 @@
 package com.redcheck.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    private boolean archived = false;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
