4970b9ea631a5148671b4530b9da968f2770f2fc
Pacolias <pacomolinac2003@gmail.com>
2026-03-03 23:21:38 +0100

    feat: SubjectResponseDTO

--- src/main/java/com/redcheck/backend/dto/response/SubjectResponseDTO.java
+++ src/main/java/com/redcheck/backend/dto/response/SubjectResponseDTO.java
@@ -1,4 +1,18 @@
 package com.redcheck.backend.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SubjectResponseDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private boolean archived;
 }
