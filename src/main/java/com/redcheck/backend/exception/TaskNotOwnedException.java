62e1dde1f39e4ea14bc670c5c189aea8a3b91c01
Pacolias <pacomolinac2003@gmail.com>
2026-03-04 20:40:04 +0100

    feat: TaskNotOwnedException

--- src/main/java/com/redcheck/backend/exception/TaskNotOwnedException.java
+++ src/main/java/com/redcheck/backend/exception/TaskNotOwnedException.java
@@ -1,7 +1,7 @@
 package com.redcheck.backend.exception;
 
 public class TaskNotOwnedException extends RuntimeException {
-    public TaskNotOwnedException(String message) {
-        super(message);
+    public TaskNotOwnedException() {
+        super("You don't have permission to modify this task");
     }
 }
