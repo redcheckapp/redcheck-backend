507bdaf7d27bb7e1557a4e7c1db8541e768ce4e0
Pacolias <pacomolinac2003@gmail.com>
2026-03-04 20:44:31 +0100

    feat: custom query methods

--- src/main/java/com/redcheck/backend/repository/TaskRepository.java
+++ src/main/java/com/redcheck/backend/repository/TaskRepository.java
@@ -3,6 +3,16 @@ package com.redcheck.backend.repository;
 import com.redcheck.backend.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
+    List<Task> findAllBySubject_User_Id(Long userId);
+    List<Task> findAllBySubject_User_IdAndCompletedDateIsNull(Long userId);
+    List<Task> findAllBySubject_User_IdAndCompletedDateIsNotNull(Long userId);
+    List<Task> findAllBySubject_User_IdAndDeadlineBeforeAndCompletedDateIsNull(Long userId, LocalDateTime now);
+
+    Optional<Task> findById(Long id);
 }
