26a6ab3257fae0634179078fc2602442d814de38
Pacolias <pacomolinac2003@gmail.com>
2026-03-04 13:03:45 +0100

    feat: SubjectNotFoundException

--- src/main/java/com/redcheck/backend/exception/SubjectNotFoundException.java
+++ src/main/java/com/redcheck/backend/exception/SubjectNotFoundException.java
@@ -1,7 +1,7 @@
 package com.redcheck.backend.exception;
 
 public class SubjectNotFoundException extends RuntimeException {
-  public SubjectNotFoundException(String message) {
-    super(message);
-  }
+    public SubjectNotFoundException(Long id) {
+        super("Subject with id '" + id + "' not found");
+    }
 }
