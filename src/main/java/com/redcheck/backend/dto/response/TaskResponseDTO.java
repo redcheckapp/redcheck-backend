a9e711d4c5ffeb584b797d46c5b4d0d5a4493a4a
Pacolias <pacomolinac2003@gmail.com>
2026-03-04 17:24:06 +0100

    feat: TaskResponseDTO

--- src/main/java/com/redcheck/backend/dto/response/TaskResponseDTO.java
+++ src/main/java/com/redcheck/backend/dto/response/TaskResponseDTO.java
@@ -1,4 +1,24 @@
 package com.redcheck.backend.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TaskResponseDTO {
+
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime assignedDate;
+    private LocalDateTime deadline;
+    private LocalDateTime completedDate;
+    private boolean completed; // it can be inferred from completedDate
+    private Long subjectId;
 }
