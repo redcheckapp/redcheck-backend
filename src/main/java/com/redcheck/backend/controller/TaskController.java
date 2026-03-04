f19f6e54415c5b480fa5ba63c2ea7a04d535a03d
Pacolias <pacomolinac2003@gmail.com>
2026-03-04 23:45:51 +0100

    feat: TaskController

--- src/main/java/com/redcheck/backend/controller/TaskController.java
+++ src/main/java/com/redcheck/backend/controller/TaskController.java
@@ -1,5 +1,78 @@
 package com.redcheck.backend.controller;
 
+import com.redcheck.backend.dto.TaskCompleteDTO;
+import com.redcheck.backend.dto.request.TaskRequestDTO;
+import com.redcheck.backend.dto.response.TaskResponseDTO;
+import com.redcheck.backend.entity.User;
+import com.redcheck.backend.service.TaskService;
+import org.springframework.web.bind.annotation.RequestBody;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/subjects/{subjectId}/tasks")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class TaskController {
-    //TODO: Create, edit, delete, mark as completed, manage overdue tasks
+
+    private final TaskService taskService;
+
+    @GetMapping
+    public ResponseEntity<List<TaskResponseDTO>> getAll(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable Long subjectId,
+            @RequestParam(required = false) Boolean completed,
+            @RequestParam(required = false) Boolean overdue){
+
+        List<TaskResponseDTO> response = taskService.getAllTask(currentUser, subjectId, completed, overdue);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskResponseDTO> create(
+            @PathVariable Long subjectId,
+            @Valid @RequestBody TaskRequestDTO requestDTO,
+            @AuthenticationPrincipal User currentUser){
+
+        TaskResponseDTO response = taskService.createTask(subjectId, requestDTO, currentUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/{taskId}")
+    public ResponseEntity<TaskResponseDTO> update(
+            @PathVariable Long subjectId,
+            @PathVariable Long taskId,
+            @Valid @RequestBody TaskRequestDTO requestDTO,
+            @AuthenticationPrincipal User currentUser){
+
+        TaskResponseDTO response = taskService.updateTask(subjectId, taskId, requestDTO, currentUser);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{taskId}")
+    public ResponseEntity<Void> delete(
+            @PathVariable Long subjectId,
+            @PathVariable Long taskId,
+            @AuthenticationPrincipal User currentUser){
+
+        taskService.deleteTask(subjectId, taskId, currentUser);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{taskId}/complete")
+    public ResponseEntity<TaskResponseDTO> complete(
+            @PathVariable Long subjectId,
+            @PathVariable Long taskId,
+            @Valid @RequestBody TaskCompleteDTO requestDTO,
+            @AuthenticationPrincipal User currentUser){
+
+        TaskResponseDTO response = taskService.markTaskAsCompleted(subjectId, taskId, requestDTO, currentUser);
+        return ResponseEntity.ok(response);
+    }
 }
