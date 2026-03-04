1a8be42f3aaa075a1b50efad699ee94b27c8ce59
Pacolias <pacomolinac2003@gmail.com>
2026-03-04 13:04:53 +0100

    feat: existsByNameAndUser and existsByNameAndUserAndIdNot

--- src/main/java/com/redcheck/backend/repository/SubjectRepository.java
+++ src/main/java/com/redcheck/backend/repository/SubjectRepository.java
@@ -1,6 +1,7 @@
 package com.redcheck.backend.repository;
 
 import com.redcheck.backend.entity.Subject;
+import com.redcheck.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +9,8 @@ import java.util.List;
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
 
     List<Subject> findAllByUserId(Long userId);
+
+    boolean existsByNameAndUser(String name, User user);
+
+    boolean existsByNameAndUserAndIdNot(String name, User user, Long id);
 }
