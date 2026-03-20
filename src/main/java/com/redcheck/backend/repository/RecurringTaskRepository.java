45f793c2e987153669b8a81ce0dd5c145e6e18ff
Pacolias <pacomolinac2003@gmail.com>
2026-03-20 13:40:51 +0100

    feat: RecurringTaskRepository implemented

---
+++ src/main/java/com/redcheck/backend/repository/RecurringTaskRepository.java
@@ -0,0 +1,12 @@
+package com.redcheck.backend.repository;
+
+import com.redcheck.backend.entity.RecurringTask;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RecurringTaskRepository extends JpaRepository<RecurringTask, Long> {
+
+    List<RecurringTask> findAllBySubject_User_IdAndActive(Long userId, Boolean active);
+    List<RecurringTask> findAllBySubject_User_Id(Long userId);
+}
