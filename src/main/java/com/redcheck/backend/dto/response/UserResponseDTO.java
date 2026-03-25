89adc4184c50d5da20cf7e1c1433e40c7b2e44e8
Pacolias <pacomolinac2003@gmail.com>
2026-03-25 12:46:17 +0100

    feat: UserResponseDTO implemented for using real username in the frontend

---
+++ src/main/java/com/redcheck/backend/dto/response/UserResponseDTO.java
@@ -0,0 +1,14 @@
+package com.redcheck.backend.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponseDTO {
+    private String username;
+}
