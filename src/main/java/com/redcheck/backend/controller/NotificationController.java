432f13a36852daffc7e1b9f1c78c00298fab26bf
Pacolias <pacomolinac2003@gmail.com>
2026-03-25 17:46:02 +0100

    feat: Notification Controller implemented

---
+++ src/main/java/com/redcheck/backend/controller/NotificationController.java
@@ -0,0 +1,29 @@
+package com.redcheck.backend.controller;
+
+import com.redcheck.backend.dto.response.NotificationResponseDTO;
+import com.redcheck.backend.entity.User;
+import com.redcheck.backend.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notifications")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @GetMapping
+    public ResponseEntity<List<NotificationResponseDTO>> getNotifications(
+            @AuthenticationPrincipal User currentUser,
+            @RequestParam(required = false) Boolean read){
+
+        List<NotificationResponseDTO> response = notificationService.getNotifications(currentUser, read);
+        return ResponseEntity.ok(response);
+    }
+}
