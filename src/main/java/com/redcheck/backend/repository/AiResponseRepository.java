69dba386ae439b0dbb427ac542f08a8a68037093
Pacolias <pacomolinac2003@gmail.com>
2026-03-26 11:47:28 +0100

    style: remove unused imports

--- src/main/java/com/redcheck/backend/repository/AiResponseRepository.java
+++ src/main/java/com/redcheck/backend/repository/AiResponseRepository.java
@@ -4,8 +4,6 @@ import com.redcheck.backend.entity.AiResponse;
 import com.redcheck.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface AiResponseRepository extends JpaRepository<AiResponse, Long> {
