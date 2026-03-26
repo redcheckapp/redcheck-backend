afbd432c478a142459dceecfd8763d06a82dda26
Pacolias <pacomolinac2003@gmail.com>
2026-03-26 11:13:45 +0100

    feat: Added 1:N relation with AiResponse

--- src/main/java/com/redcheck/backend/entity/User.java
+++ src/main/java/com/redcheck/backend/entity/User.java
@@ -45,6 +45,9 @@ public class User implements UserDetails {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProgressRecord> notifications = new ArrayList<>();
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProgressRecord> aiResponses = new ArrayList<>();
+
     public String getActualUsername(){
         return username;
     }
