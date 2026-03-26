f4c1e195725b8ab9ff50f77b3221b3ecf59f1034
Pacolias <pacomolinac2003@gmail.com>
2026-03-26 18:51:06 +0100

    feat: NotificationRepository implemented

---
+++ src/main/java/com/redcheck/backend/repository/NotificationRepository.java
@@ -0,0 +1,13 @@
+package com.redcheck.backend.repository;
+
+import com.redcheck.backend.entity.Notification;
+import com.redcheck.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findAllByUserAndRead(User user, boolean read);
+    List<Notification> findAllByUser(User user);
+}
