74392e51ec728c27492bf6bf8197f0e85d3d6d87
Pacolias <pacomolinac2003@gmail.com>
2026-03-27 22:13:08 +0100

    feat: findPendingOrCompletedToday JPQL query added

--- src/main/java/com/redcheck/backend/repository/TaskRepository.java
+++ src/main/java/com/redcheck/backend/repository/TaskRepository.java
@@ -17,6 +17,14 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findAllBySubject_User_IdAndCompletedDateIsNull(Long userId);
     List<Task> findAllBySubject_User_IdAndCompletedDateIsNotNull(Long userId);
     List<Task> findAllBySubject_User_IdAndDeadlineBeforeAndCompletedDateIsNull(Long userId, LocalDateTime now);
+
+    @Query("SELECT t FROM Task t WHERE t.subject.user.id = :userId AND (t.completedDate IS NULL OR (t.completedDate >= :startOfDay AND t.completedDate <= :endOfDay))")
+    List<Task> findPendingOrCompletedToday(
+            @Param("userId") Long userId,
+            @Param("startOfDay") LocalDateTime startOfDay,
+            @Param("endOfDay") LocalDateTime endOfDay
+    );
+
     Optional<Task> findTopByRecurringTaskOrderByAssignedDateDesc(RecurringTask recurringTask);
     Optional<Task> findById(Long id);
 
