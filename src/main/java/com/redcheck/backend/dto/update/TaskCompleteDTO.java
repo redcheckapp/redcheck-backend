fbc74550bc9cbcfd46c6af135b78851727a2ac30
Pacolias <pacomolinac2003@gmail.com>
2026-03-31 00:14:38 +0200

    feat: added @Builder annotation

--- src/main/java/com/redcheck/backend/dto/update/TaskCompleteDTO.java
+++ src/main/java/com/redcheck/backend/dto/update/TaskCompleteDTO.java
@@ -2,12 +2,14 @@ package com.redcheck.backend.dto.update;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class TaskCompleteDTO {
 
     @NotNull
