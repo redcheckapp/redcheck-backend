3c140216be44d6045adf9d3b706cbefc241a1e95
Pacolias <pacomolinac2003@gmail.com>
2026-08-26 19:16:47 +0200

    fix: multi-language bug

--- src/main/java/com/redcheck/backend/config/DataInitializer.java
+++ src/main/java/com/redcheck/backend/config/DataInitializer.java
@@ -18,17 +18,27 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // Checks if the user already exists so that we dont duplicate it
-        if (!userRepository.existsByEmail("demo@redcheck.com")) {
-            User demoUser = new User();
-            demoUser.setUsername("Usuario Demo");
-            demoUser.setEmail("demo@redcheck.com");
-            // Same password as React
-            demoUser.setPassword(passwordEncoder.encode("demo1234"));
-            demoUser.setCreationDate(LocalDateTime.now());
 
-            userRepository.save(demoUser);
-            System.out.println("Usuario de demostración creado con éxito.");
+        if (!userRepository.existsByEmail("demo-es@redcheck.com")) {
+            User demoUserEs = new User();
+            demoUserEs.setUsername("Usuario Demo");
+            demoUserEs.setEmail("demo-es@redcheck.com");
+            demoUserEs.setPassword(passwordEncoder.encode("demo1234"));
+            demoUserEs.setCreationDate(LocalDateTime.now());
+
+            userRepository.save(demoUserEs);
+            System.out.println("Usuario de demostración (ES) creado con éxito.");
+        }
+
+        if (!userRepository.existsByEmail("demo-en@redcheck.com")) {
+            User demoUserEn = new User();
+            demoUserEn.setUsername("Demo User");
+            demoUserEn.setEmail("demo-en@redcheck.com");
+            demoUserEn.setPassword(passwordEncoder.encode("demo1234"));
+            demoUserEn.setCreationDate(LocalDateTime.now());
+
+            userRepository.save(demoUserEn);
+            System.out.println("Usuario de demostración (EN) creado con éxito.");
         }
     }
 }
