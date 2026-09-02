ad17fee8cda2e3ae9c20871f86781e3c9d05b42c
Pacolias <pacomolinac2003@gmail.com>
2026-09-02 19:15:54 +0200

    chore: Add newline for consistency in SubjectNotOwnedException.java

--- src/main/java/com/redcheck/backend/exception/SubjectNotOwnedException.java
+++ src/main/java/com/redcheck/backend/exception/SubjectNotOwnedException.java
@@ -1,6 +1,7 @@
 package com.redcheck.backend.exception;
 
 public class SubjectNotOwnedException extends RuntimeException {
+
     public SubjectNotOwnedException() {
         super("You don't have permission to modify this subject");
     }
