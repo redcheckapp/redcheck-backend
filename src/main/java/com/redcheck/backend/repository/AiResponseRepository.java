c960b018fedc6f3c0c4a6363c1512d669f03cfcb
Pacolias <pacomolinac2003@gmail.com>
2026-09-02 19:12:27 +0200

    chore: Add newline at end of AiResponseRepository.java for consistency

--- src/main/java/com/redcheck/backend/repository/AiResponseRepository.java
+++ src/main/java/com/redcheck/backend/repository/AiResponseRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AiResponseRepository extends JpaRepository<AiResponse, Long> {
+
     Optional<AiResponse> findFirstByUserAndTypeOrderByCreatedDateDesc(User user, AiResponse.Type type);
 }
