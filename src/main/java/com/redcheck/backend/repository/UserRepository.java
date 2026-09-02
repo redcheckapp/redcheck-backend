4c054e28958e93adbd5706aaa71ac3a05f577ab0
Pacolias <pacomolinac2003@gmail.com>
2026-09-02 19:12:50 +0200

    chore: Add newlines for consistency in UserRepository.java

--- src/main/java/com/redcheck/backend/repository/UserRepository.java
+++ src/main/java/com/redcheck/backend/repository/UserRepository.java
@@ -2,9 +2,10 @@ package com.redcheck.backend.repository;
 
 import com.redcheck.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
