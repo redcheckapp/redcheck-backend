27cbc1094a976f532246da10f2e569d54c677116
Pacolias <pacomolinac2003@gmail.com>
2026-09-02 19:02:16 +0200

    chore: Enhance logging in ProgressRecordSchedulerService for daily record generation

--- src/main/java/com/redcheck/backend/service/ProgressRecordSchedulerService.java
+++ src/main/java/com/redcheck/backend/service/ProgressRecordSchedulerService.java
@@ -2,9 +2,11 @@ package com.redcheck.backend.service;
 
 import com.redcheck.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProgressRecordSchedulerService {
@@ -13,7 +15,9 @@ public class ProgressRecordSchedulerService {
     private final ProgressRecordService progressRecordService;
 
     @Scheduled(cron = "0 59 23 * * *")
-    public void generateDailyProgressRecords(){
+    public void generateDailyProgressRecords() {
+        log.info("Starting generation of daily progress records for all users...");
         userRepository.findAll().forEach(progressRecordService::generateDailyRecord);
+        log.info("Daily progress records generation completed.");
     }
 }
